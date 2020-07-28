0e750270709feb2e38f843b358e2b935e7194fec
John Lonetree <jlonetree@gmail.com>
2020-07-28 00:31:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,12 +6,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am leanring to program in #{language}"
 end
 
-
-
-
-
-
-
+introduction_with_language("Dan", "Ember.js")
 
 
 
